2c46703228bcf5e9ca143049bde1b750b5d4f3bb
kpodlaski <podlaski@uni.lodz.pl>
2023-12-02 15:02:55 +0100

    v2

--- zaoczne/springJPAandResr/src/main/java/springtest/CustomerController.java
+++ zaoczne/springJPAandResr/src/main/java/springtest/CustomerController.java
@@ -1,13 +1,30 @@
 package springtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
 
+    @Autowired
+    CustomerRepository customerRepsitory;
+
+    @GetMapping ("/init_db")
+    public String initDb(){
+        customerRepsitory.save(new Customer("Jack", "Bauer"));
+        customerRepsitory.save(new Customer("Chloe", "O'Brian"));
+        customerRepsitory.save(new Customer("Kim", "Bauer"));
+        customerRepsitory.save(new Customer("David", "Palmer"));
+        customerRepsitory.save(new Customer("Michelle", "Dessler"));
+        return "init_db";
+    }
+
     @GetMapping("/customer")
-    public String getAll(){
+    public String getAll(Model model){
+        Customer customer = customerRepsitory.findById(1);
+        model.addAttribute("cust",customer);
         return "customer_view";
     }
 }
